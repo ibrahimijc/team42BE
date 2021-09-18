ac7856c42c851924c18abc2c019cdef03b989bb4
Muhammad Ibrahim <muhammad.ibrahim@ids-astra.com>
2021-09-18 15:45:51 +0500

    pushed backend code

---
+++ server/middlewear/Auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const Models = require('../models/Employee');
+const { Customer } = Models;
+
+const authCustomer = async function (req, res, next) {
+
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decode = await jwt.verify(token, process.env.secret);
+
+        const customer = await Customer.findOne({ _id: decode._id, 'tokens.token': token});
+        if (!customer) {
+            throw new Error();
+        }
+
+        req.token = token;
+        req.customer = customer;
+        next();
+    } catch (e) {
+        res.status(401).send({ error: 'please authenticate' });
+    }
+}
+
+const authRider = async function (req, res, next) {
+    next()
+}
+
+module.exports = { authCustomer, authRider };
